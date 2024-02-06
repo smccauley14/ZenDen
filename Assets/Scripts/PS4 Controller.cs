31b491e1afea16a6034e7ea0deb919942562e493
dmclaughlin104 <dmclaughlin104@gmail.com>
2024-02-06 21:19:32 +0000

    Update with notes to controller script
    Added notes to controller script

--- Assets/Scripts/PS4 Controller.cs
+++ Assets/Scripts/PS4 Controller.cs
@@ -10,17 +10,22 @@ public class PS4Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //printing to log to check controller/s are connected
         for (int i =0; i< Gamepad.all.Count; i++)
         {
             Debug.Log(Gamepad.all[i].name);
         }
 
+        //find cube object
         cube = GameObject.Find("Cube");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //applying basic movement controls to the left joystick of PS controller
+        //forward, backward, side-to-side
+        //NB. currently using a bool (isPressed) rather than getting joystick axis
         if (Gamepad.all.Count > 0)
         {
             if (Gamepad.all[0].leftStick.left.isPressed)
