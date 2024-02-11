ae5f6b42e6e3113db64190d73b04ce82dbb59e8d
dmclaughlin104 <dmclaughlin104@gmail.com>
2024-02-11 18:36:02 +0000

    New direction sticks introduced
    New direction sticks introduced

--- Assets/DirectionButtons.cs
+++ Assets/DirectionButtons.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using UnityEngine.InputSystem;
 
 public class DirectionButtons : MonoBehaviour
 {
-    public float movementSpeed = 0.1f;
+    private float movementSpeed = 5f;
+    private float rotationSpeed = 60f;
     public Button forwardButton;
 
 
@@ -21,26 +23,51 @@ public class DirectionButtons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        forwardButton.onClick.AddListener(forwardMovement);
 
-       //forwardButton.OnPointerDown();
-
-
-
-    }
-
-    /*
-    private bool forwardButtonPressed()
-    {
-
-        if (forwardButton.onClick)
+        //FORWARD BUTTON
+        if (Gamepad.all[0].dpad.up.isPressed)
         {
-            return true;
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
         }
 
-        return false;
+
+        //LEFT STICK CONTROLLER:
+
+        //left movement
+        if (Gamepad.all[0].leftStick.left.isPressed)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * movementSpeed);
+        }
+        //right movement
+        if (Gamepad.all[0].leftStick.right.isPressed)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
+        }
+        //forward movemment
+        if (Gamepad.all[0].leftStick.up.isPressed)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+        }
+        //backward movement
+        if (Gamepad.all[0].leftStick.down.isPressed)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * movementSpeed);
+        }
+
+        //RIGHT JOYSTICK
+        //right and left rotation
+        if (Gamepad.all[0].rightStick.right.isPressed)
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+        }
+
+        if (Gamepad.all[0].rightStick.left.isPressed)
+        {
+            transform.Rotate(Vector3.down, Time.deltaTime * rotationSpeed);
+        }
+
     }
-    */
+
 
     private void forwardMovement()
     {
