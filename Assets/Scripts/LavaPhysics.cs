2a09516a35b9c5750b20a886c9c6808c41372175
mmalcolm02 <mmalcolm02@qub.ac.uk>
2024-02-20 10:32:13 +0000

    Lava Lamp Object
    Lava Blobs with physics added inside the tube. Would be better if they were metaballs but not possible within the timeframe...

--- Assets/Scripts/LavaPhysics.cs
+++ Assets/Scripts/LavaPhysics.cs
@@ -14,48 +14,47 @@ public class LavaPhysics : MonoBehaviour
     public float heatDistance;
     public float bouyancyConversion = 0.01f;
     public float coolingConstant = 0.5f;
+    public float maxHeight = 2.0f;
+    public float minHeight = 0.0f;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //lava balls rise when hot, cool, then fall. Heatsource heats balls, cooling proportioanl to a passive constant plus an increased amount proportional temperature.
     }
 
 
     private void FixedUpdate()
     {
-        //don't fall below heat source
-        // if (transform.position.y < heatSourceY)
-        //   {
-        //     transform.position = new Vector3(transform.position.x, heatSourceY, transform.position.z);
-        //  }
+        
 
         //distance to heat source
         heatDistance = Mathf.Abs(transform.position.y - heatSourceY);
 
-        //heating of the game object
-        // temperature += ((heatInfluence * (1/heatDistance)) - passiveCooling);
-
-
-        //impact of heat on velocity
-        //  velocity += new Vector3 (0,(temperature * bouyancy) + gravity,0);
-
-        //velocity movement
-        // transform.localPosition += (velocity * Time.deltaTime);
-
+        //setting movement proportional to gravity and bouyancy
         velocity = new Vector3(0, (bouyancy-gravity)*Time.deltaTime, 0);
         transform.localPosition += (velocity * Time.deltaTime);
+
+        //determining rate of cooling of lava
         passiveCooling = 0.05f + temperature * coolingConstant;
 
-        if (transform.position.y < 0.1)
+        //setting lava ball height restrictions
+        if (transform.position.y < 0)
         {
-        transform.position = new Vector3(transform.position.x, 0.1f, transform.position.z);
+        transform.position = new Vector3(transform.position.x, minHeight, transform.position.z);
         }
 
+        if (transform.position.y > 2)
+        {
+            transform.position = new Vector3(transform.position.x, maxHeight, transform.position.z);
+        }
+
+        //temperature proportional to inverse square of distance to heat source
         temperature += (heatInfluence * (1 / heatDistance) * (1 / heatDistance)) - passiveCooling;
 
+        //bouyancy influenced by temperature
         bouyancy += bouyancyConversion * temperature;
     }
 }
